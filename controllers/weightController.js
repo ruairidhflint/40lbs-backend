0060aa035093ca6e506d45d7ca61ab42f3ce6131
Rory Flint <roryflintphoto@gmail.com>
2020-02-05 08:15:08 +0000

    Refactor file structure

--- controllers/weightController.js
+++ controllers/weightController.js
@@ -16,6 +16,40 @@ async function postNewWeight(req, res) {
   }
 }
 
+async function getRecentWeightsById(req, res) {
+  try {
+    const weights = await helpers.getRecentWeightsById(req.user.id);
+    if (!weights) {
+      res
+        .status(400)
+        .json({ message: 'There was a problem retrieivng your data' });
+    }
+    res.status(200).json({ weights });
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: 'There was a problem retrieivng your data', error });
+  }
+}
+
+async function getAllWeightsById(req, res) {
+  try {
+    const weights = await helpers.getAllWeightsById(req.user.id);
+    if (!weights) {
+      res
+        .status(400)
+        .json({ message: 'There was a problem retrieivng your data' });
+    }
+    res.status(200).json({ weights });
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: 'There was a problem retrieivng your data', error });
+  }
+}
+
 module.exports = {
   postNewWeight,
+  getAllWeightsById,
+  getRecentWeightsById,
 };
--- routes/weightRoute.js
+++ routes/weightRoute.js
@@ -2,7 +2,6 @@ const express = require('express');
 const authMiddleware = require('../middleware/authMiddleware');
 const weightMiddleware = require('../middleware/weightMiddleware');
 const weightController = require('../controllers/weightController');
-const weightHelper = require('../helpers/weightHelpers');
 
 const Router = express.Router();
 
@@ -16,36 +15,16 @@ Router.post(
   weightController.postNewWeight,
 );
 
-Router.get('/user', [authMiddleware.validateUser], async (req, res) => {
-  try {
-    const weights = await weightHelper.getRecentWeightsById(req.user.id);
-    if (!weights) {
-      res
-        .status(400)
-        .json({ message: 'There was a problem retrieivng your data' });
-    }
-    res.status(200).json({ weights });
-  } catch (error) {
-    res
-      .status(500)
-      .json({ message: 'There was a problem retrieivng your data', error });
-  }
-});
+Router.get(
+  '/user',
+  [authMiddleware.validateUser],
+  weightController.getAllWeightsById,
+);
 
-Router.get('/user/all', [authMiddleware.validateUser], async (req, res) => {
-  try {
-    const weights = await weightHelper.getAllWeightsById(req.user.id);
-    if (!weights) {
-      res
-        .status(400)
-        .json({ message: 'There was a problem retrieivng your data' });
-    }
-    res.status(200).json({ weights });
-  } catch (error) {
-    res
-      .status(500)
-      .json({ message: 'There was a problem retrieivng your data', error });
-  }
-});
+Router.get(
+  '/user/all',
+  [authMiddleware.validateUser],
+  weightController.getAllWeightsById,
+);
 
 module.exports = Router;
